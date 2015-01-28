2599b85386e1b52faa89e949301c516c9ba62edb
sam0hack <sam.nyx@live.com>
2015-01-29 03:49:17 +0530

    Basic Configuration

--- app/Config/core.php
+++ app/Config/core.php
@@ -222,12 +222,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'DYhGqwewq93b0qyJfIxfs2sdfdsguVoUubWwvnhghgiR2G0FgauyyuuyC9uyumi');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '7685845151545439309657454353153542495434351545576749683645');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
